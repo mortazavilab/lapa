6bb5a8cbcc8117ce3d8a50a74bff5e8c48608c47
M. Hasan Celik <muhammedhasancelik@gmail.com>
2021-12-29 22:03:15 -0800

    lapa wtc11

--- lapa/main.py
+++ lapa/main.py
@@ -8,18 +8,17 @@ from lapa.lapa import lapa
 @click.option('--annotation', help='Standart transcriptome Annotation (Encode or Ensembl gtf)')
 @click.option('--chrom_sizes', help='Chrom sizes files (can be generated with `faidx fasta -i chromsizes > chrom_sizes`)')
 @click.option('--output_dir', help='Output directory')
-@click.option('--method', help='Counting method either `end` or `tail`',
-              type=click.Choice(['end', 'tail'], case_sensitive=True),
-              default=None)
+@click.option('--counting_method', help='Counting method either `end` or `tail`',
+              type=click.Choice(['end', 'tail'], case_sensitive=True), default='end')
 @click.option('--min_tail_len', default=10, type=int)
 @click.option('--min_percent_a', default=0.9, type=float)
 @click.option('--mapq', default=10, type=int)
 @click.option('--cluster_extent_cutoff', default=3, type=int)
 @click.option('--cluster_window', default=25, type=int)
-def cli(alignment, fasta, annotation, chrom_sizes, output_dir, method,
+def cli(alignment, fasta, annotation, chrom_sizes, output_dir, counting_method,
         min_tail_len=10, min_percent_a=0.9, mapq=10,
         cluster_extent_cutoff=3, cluster_window=25):
-    lapa(alignment, fasta, annotation, chrom_sizes, output_dir, method,
+    lapa(alignment, fasta, annotation, chrom_sizes, output_dir, counting_method,
          min_tail_len=min_tail_len, min_percent_a=min_percent_a, mapq=mapq)
 
 
