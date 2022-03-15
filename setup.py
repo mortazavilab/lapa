fd20eb159ea552cf70418abf6a0da486e88e3e88
M. Hasan Celik <muhammedhasancelik@gmail.com>
2022-03-15 16:06:33 -0700

    setup fix

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
 requirements = [
     'setuptools',
     'tqdm',
@@ -20,26 +24,38 @@ setup_requirements = ['pytest-runner', ]
 test_requirements = ['pytest']
 
 setup(
+    name='lapa',
+    version='0.0.1',
+
     author="M. Hasan Çelik",
     author_email='muhammedhasancelik@gmail.com',
+    url='https://github.com/mortazavilab/lapa',
+
+    keywords=['genomics', 'long read RNA-seq', 'APA'],
+    description="Alternative polyadenylation detection from diverse data sources",
+
     classifiers=[
         'Natural Language :: English',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
     ],
-    description="Alternative polyadenylation detection from diverse data sources",
-    install_requires=requirements,
     license="MIT license",
+    long_description=readme + '\n',
+    long_description_content_type='text/markdown',
+
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    
     entry_points='''
         [console_scripts]
         lapa=lapa.main:cli
         lapa_correct_talon_gtf=lapa.main:lapa_correct_talon_gtf
     ''',
-    keywords=['genomics', 'long read RNA-seq', 'APA'],
-    name='lapa',
     packages=find_packages(include=['lapa']),
-    setup_requires=setup_requirements,
+    include_package_data=True,
+
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/mortazavilab/lapa',
-    version='0.0.1',
 )
---
+++ workflow/README.md
@@ -0,0 +1,19 @@
+
+# Download
+
+
+Download color library metbrewer:
+
+```
+git clone https://github.com/BlakeRMills/MetBrewer
+cd MetBrewer/Python
+pip install .
+```
+
+
+# Run 
+
+Run snakemake from the root directory of the repo:
+```
+python -m snakemake -j 1
+```
